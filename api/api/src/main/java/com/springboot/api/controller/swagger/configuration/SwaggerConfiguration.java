d556f86f3c527fefe537b3d22dc23c6431471ca3
jh4dev <jh4dev@gmail.com>
2024-07-24 21:06:03 +0900

    springfox to springdoc
    springfox 제거 및 springdoc 적용

--- api/api/src/main/java/com/springboot/api/controller/swagger/configuration/SwaggerConfiguration.java
+++ api/api/src/main/java/com/springboot/api/controller/swagger/configuration/SwaggerConfiguration.java
@@ -1,37 +1,27 @@
 package com.springboot.api.controller.swagger.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springboot.api"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-
-        return new ApiInfoBuilder()
-                .title("Spring Boot API Test with Swagger")
-                .description("설명설명")
-                .version("1.0.0")
-                .build();
+    private Info apiInfo() {
+        return new Info()
+                .title("Springdoc Swagger")
+                .description("Springdoc을 사용한 Swagger UI")
+                .version("1.0.0");
     }
 }
